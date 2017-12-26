c7e8bb6bb858d14e80a8bd261b83b3cfca75f6a1
Alex Sagel <alex.sagel7587@gmail.com>
2017-12-26 20:48:44 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+export class RegistrationForm extends React.Component {
+  render(){
+    return (
+      <div className="wrapper">
+        <form>
+          
+        </form>
+      </div>
+    )
+  }
+}
